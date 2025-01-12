e802c2a33c893dd7cc412dca536ebee368e663e9
heli-d-bhavsar <heli.d.bhavsar@gmail.com>
2025-01-12 08:13:56 -0400

    added mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,67 @@
+import { useState } from 'react';
+import Sidebar from 'react-sidebar';
+
 const Header = () => {
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+
+  const handleSetSidebarOpen = (open: boolean): void => {
+    setSidebarOpen(open);
+  };
   return (
-    <header>
-      <div className='max-w-7xl h-[20vh] mx-auto'>
-        <div className='flex items-center justify-between text-center'>
-          <div className='font-logo text-[52px]'>FASCO</div>
-          <div>
-            <ul className='flex gap-7 text-base items-center justify-center'>
-              <li>Home</li>
-              <li>Deals</li>
-              <li>New Arrivals</li>
-              <li>Packages</li>
-              <li>Sign in</li>
-              <li>
-                <button className='bg-black text-white w-[152px] h-[56px] rounded-xl'>
-                  Sign up
-                </button>
-              </li>
-            </ul>
+    <>
+      <div className='block md:hidden'>
+        <Sidebar
+          sidebar={
+            <div className='flex flex-col p-5'>
+              <div className='font-logo text-[52px]'>FASCO</div>
+              <div>
+                <ul className='flex flex-col gap-7 text-base items-center justify-center'>
+                  <li>Home</li>
+                  <li>Deals</li>
+                  <li>New Arrivals</li>
+                  <li>Packages</li>
+                  <li>Sign in</li>
+                  <li>
+                    <button className='bg-black text-white w-[152px] h-[56px] rounded-xl'>
+                      Sign up
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          }
+          open={sidebarOpen}
+          onSetOpen={handleSetSidebarOpen}
+          styles={{ sidebar: { background: 'white' } }}
+        >
+          <button onClick={() => handleSetSidebarOpen(true)}>
+            Open sidebar
+          </button>
+        </Sidebar>
+      </div>
+
+      <header className='hidden md:block'>
+        <div className='max-w-7xl h-[20vh] mx-auto'>
+          <div className='flex items-center justify-between text-center'>
+            <div className='font-logo text-[52px]'>FASCO</div>
+            <div>
+              <ul className='flex gap-7 text-base items-center justify-center'>
+                <li>Home</li>
+                <li>Deals</li>
+                <li>New Arrivals</li>
+                <li>Packages</li>
+                <li>Sign in</li>
+                <li>
+                  <button className='bg-black text-white w-[152px] h-[56px] rounded-xl'>
+                    Sign up
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
