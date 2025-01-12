bd14dbd9360a2eda8225e65871e6206a925a25a4
heli-d-bhavsar <heli.d.bhavsar@gmail.com>
2025-01-11 22:03:24 -0400

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <header>
+      <div className='max-w-7xl h-[20vh] mx-auto'>
+        <div className='flex items-center justify-between text-center'>
+          <div className='font-logo text-[52px]'>FASCO</div>
+          <div>
+            <ul className='flex gap-7 text-base items-center justify-center'>
+              <li>Home</li>
+              <li>Deals</li>
+              <li>New Arrivals</li>
+              <li>Packages</li>
+              <li>Sign in</li>
+              <li>
+                <button className='bg-black text-white w-[152px] h-[56px] rounded-xl'>
+                  Sign up
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
