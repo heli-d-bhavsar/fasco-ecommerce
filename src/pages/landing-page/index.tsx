368e75435b94327ae48eb267634795eae4dd9f28
heli-d-bhavsar <heli.d.bhavsar@gmail.com>
2025-01-14 07:42:58 -0400

    added padding

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -3,7 +3,7 @@ import HeroBanner from '../../components/landing-page/HeroBanner';
 
 const LandingPage = () => {
   return (
-    <div className='max-w-7xl h-[90vh] mx-auto'>
+    <div className='max-w-7xl h-[90vh] mx-auto px-4 md:px-2'>
       <HeroBanner />
       <BrandList />
     </div>
