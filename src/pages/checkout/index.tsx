5882cd4e4cf5f5c555c7b637eb6cda965d6587bc
heli-d-bhavsar <heli.d.bhavsar@gmail.com>
2025-02-08 08:18:29 -0400

    added validation

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,96 +1,124 @@
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import valid from 'card-validator';
 
 const Checkout = () => {
-  const checkOutFormValues = {
-    email: '',
-    firstName: '',
-    lastName: '',
-    phoneNumber: '',
-    address: '',
-    country: '',
-    city: '',
-    postalCode: '',
-    cardNumber: '',
-    expiryDate: '',
-    cardHolderName: '',
-    securityCode: '',
-  };
+  const validationSchema = Yup.object({
+    firstName: Yup.string()
+      .min(3, 'Must be at least 3 characters')
+      .required('Name is required'),
+    lastName: Yup.string()
+      .min(3, 'Must be at least 3 characters')
+      .required('Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    phoneNumber: Yup.string().required('Phone number is required'),
+    address: Yup.string()
+      .required('Address is required')
+      .min(3, 'Must be at least 3 characters'),
+    country: Yup.string().required('Country is required'),
+    city: Yup.string().required('City is required'),
+    postalCode: Yup.string()
+      .required('Postal Code is required')
+      .min(3, 'Must be at least 3 characters'),
+    cardHolderName: Yup.string()
+      .required('Card holder name is required')
+      .min(3, 'Must be at least 3 characters'),
+    cardNumber: Yup.string().test(
+      'test-number',
+      'Credit Card number is invalid',
+      (value) => valid.number(value).isValid
+    ),
+    expiryDate: Yup.string()
+      .required('Expiry date is required')
+      .matches(/^(0[1-9]|1[0-2])\/\d{2}$/g, 'Invalid expiration date'),
+    securityCode: Yup.string()
+      .min(3, 'Must be at least 3 characters')
+      .max(4)
+      .required(),
+  });
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      address: '',
+      country: '',
+      city: '',
+      postalCode: '',
+      cardNumber: '',
+      expiryDate: '',
+      cardHolderName: '',
+      securityCode: '',
+    },
+    validationSchema,
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div>
-      <Formik
-        initialValues={checkOutFormValues}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <h2 className='font-logo text-5xl my-5'>Contact</h2>
+    <div className='flex'>
+      <div className='w-1/2 '>
+        <form onSubmit={formik.handleSubmit}>
+          <h2 className='font-logo text-3xl my-5 text-[#484848]'>Contact</h2>
 
-            <label
-              className='block text-gray-700 text-sm font-semibold mb-1'
-              htmlFor='username'
-            >
-              Email
-            </label>
-            <input
-              type='email'
-              name='email'
-              className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              placeholder='Enter Email'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
+          <label
+            className='block text-gray-700 text-sm font-semibold mb-1'
+            htmlFor='username'
+          >
+            Email
+          </label>
+          <input
+            type='email'
+            name='email'
+            className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+            placeholder='Enter Email'
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.touched && formik.errors.email ? formik.errors.email : null}
 
-            <h2 className='font-logo text-5xl my-5'>Delivery</h2>
+          <h2 className='font-logo text-3xl my-5 text-[#484848]'>Delivery</h2>
 
-            <div className='flex'>
-              <div className='w-1/2'>
-                <label
-                  className='block text-gray-700 text-sm font-semibold mb-1'
-                  htmlFor='firstName'
-                >
-                  First Name
-                </label>
-                <input
-                  type='firstName'
-                  name='text'
-                  className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.firstName}
-                />
-              </div>
-
-              <div className='w-1/2'>
-                <label
-                  className='block text-gray-700 text-sm font-semibold mb-1'
-                  htmlFor='lastName'
-                >
-                  Last Name
-                </label>
-                <input
-                  type='lastName'
-                  name='text'
-                  className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.lastName}
-                />
-              </div>
+          <div className='flex gap-3 mb-4'>
+            <div className='w-1/2'>
+              <label
+                className='block text-gray-700 text-sm font-semibold mb-1'
+                htmlFor='firstName'
+              >
+                First Name
+              </label>
+              <input
+                name='firstName'
+                type='text'
+                placeholder='Enter First Name'
+                className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+              />
             </div>
 
+            <div className='w-1/2'>
+              <label
+                className='block text-gray-700 text-sm font-semibold mb-1'
+                htmlFor='lastName'
+              >
+                Last Name
+              </label>
+              <input
+                name='lastName'
+                type='text'
+                placeholder='Enter Last Name'
+                className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+              />
+            </div>
+          </div>
+
+          <div className='mb-4'>
             <label
               className='block text-gray-700 text-sm font-semibold mb-1'
               htmlFor='address'
@@ -98,51 +126,52 @@ const Checkout = () => {
               Address
             </label>
             <input
-              type='address'
-              name='text'
-              className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.address}
+              name='address'
+              type='text'
+              placeholder='Enter Address'
+              className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+              onChange={formik.handleChange}
+              value={formik.values.address}
             />
+          </div>
 
-            <div className='flex'>
-              <div className='w-1/2'>
-                <label
-                  className='block text-gray-700 text-sm font-semibold mb-1'
-                  htmlFor='city'
-                >
-                  City
-                </label>
-                <input
-                  type='city'
-                  name='text'
-                  className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.city}
-                />
-              </div>
-              <div className='w-1/2'>
-                <label
-                  className='block text-gray-700 text-sm font-semibold mb-1'
-                  htmlFor='postalCode'
-                >
-                  Postal Code
-                </label>
-                <input
-                  type='postalCode'
-                  name='text'
-                  className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.postalCode}
-                />
-              </div>
+          <div className='flex gap-3 mb-4'>
+            <div className='w-1/2'>
+              <label
+                className='block text-gray-700 text-sm font-semibold mb-1'
+                htmlFor='city'
+              >
+                City
+              </label>
+              <input
+                name='city'
+                type='text'
+                placeholder='Enter City'
+                className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+                onChange={formik.handleChange}
+                value={formik.values.city}
+              />
             </div>
+            <div className='w-1/2'>
+              <label
+                className='block text-gray-700 text-sm font-semibold mb-1'
+                htmlFor='postalCode'
+              >
+                Postal Code
+              </label>
+              <input
+                name='postalCode'
+                type='text'
+                placeholder='Enter Postal Code'
+                className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+                onChange={formik.handleChange}
+                value={formik.values.postalCode}
+              />
+            </div>
+          </div>
 
-            <h2 className='font-logo text-5xl my-5'>Payment</h2>
-
+          <h2 className='font-logo text-3xl my-5 text-[#484848]'>Payment</h2>
+          <div className='mb-4'>
             <label
               className='block text-gray-700 text-sm font-semibold mb-1'
               htmlFor='cardNumber'
@@ -150,55 +179,54 @@ const Checkout = () => {
               Card Number
             </label>
             <input
-              type='cardNumber'
-              name='text'
-              className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.cardNumber}
+              name='cardNumber'
+              type='text'
+              placeholder='Enter Card Number'
+              className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+              onChange={formik.handleChange}
+              value={formik.values.cardNumber}
             />
+          </div>
 
-            <div className='flex'>
-              <div className='w-1/2'>
-                <label
-                  className='block text-gray-700 text-sm font-semibold mb-1'
-                  htmlFor='expiryDate'
-                >
-                  Expiration Date
-                </label>
-                <input
-                  type='expiryDate'
-                  name='text'
-                  className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.expiryDate}
-                />
-              </div>
-              <div className='w-1/2'>
-                <label
-                  className='block text-gray-700 text-sm font-semibold mb-1'
-                  htmlFor='securityCode'
-                >
-                  Security Code
-                </label>
-                <input
-                  type='securityCode'
-                  name='text'
-                  className='shadow text-base appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.securityCode}
-                />
-              </div>
+          <div className='flex gap-3'>
+            <div className='w-1/2'>
+              <label
+                className='block text-gray-700 text-sm font-semibold mb-1'
+                htmlFor='expiryDate'
+              >
+                Expiration Date
+              </label>
+              <input
+                type='date'
+                name='expiryDate'
+                placeholder='Expiry Date'
+                className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+                onChange={formik.handleChange}
+                value={formik.values.expiryDate}
+              />
             </div>
+            <div className='w-1/2'>
+              <label
+                className='block text-gray-700 text-sm font-semibold mb-1'
+                htmlFor='securityCode'
+              >
+                Security Code
+              </label>
+              <input
+                name='securityCode'
+                type='text'
+                placeholder='Security Code'
+                className='text-sm appearance-none border w-full py-4 px-6 text-gray-700 leading-tight '
+                onChange={formik.handleChange}
+                value={formik.values.securityCode}
+              />
+            </div>
+          </div>
 
-            <button type='submit' disabled={isSubmitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Formik>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+      <div className='w-1/2'></div>
     </div>
   );
 };
